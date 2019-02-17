418906199fabf585f437dcf20af89634831e66cf
Kirill <kirill1098@gmail.com>
2019-02-18 01:07:39 +0300

    + кнопка на добавление во 2 активити

--- app/src/main/java/com/blogspot/atifsoftwares/imagetotextapp/Main2Activity.java
+++ app/src/main/java/com/blogspot/atifsoftwares/imagetotextapp/Main2Activity.java
@@ -53,10 +53,16 @@ public class Main2Activity extends AppCompatActivity {
 
     String _Name = "", _Company = "", _Email = "", _Telephone = "", _URL = "",_Subject = "";
 
-    public ImageView imgV;
-    public TextView txt;
+
     int idtext = 4000;
     int idspinn = 3000;
+    ImageView mPreviewIv;
+
+    String fName;
+
+    final String[] types = new String[] {
+            NAME, SUBJECT, COMPANY, EMAIL, TELEPHONE, URL, "Delete"
+    };
 
     public LinearLayout ll;
 
@@ -68,24 +74,21 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Intent intent = getIntent();
-        String fName = intent.getStringExtra("fname");
+        fName = intent.getStringExtra("fname");
         Uri myUri = Uri.parse(intent.getStringExtra("imageUri"));
 
-        ImageView mPreviewIv;
         mPreviewIv = findViewById(R.id.imageIv);
+
         ll = (LinearLayout)findViewById(R.id.liner_for_edittext);
+
         for (String retval : fName.split("\n")) {
+
             LinearLayout horisont_liner = new LinearLayout(this);
             horisont_liner.setOrientation(LinearLayout.HORIZONTAL);
             horisont_liner.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
 
             Spinner spin = new Spinner(this);
             spin.setId(idspinn++);
-
-
-            final String[] types = new String[] {
-                    NAME, SUBJECT, COMPANY, EMAIL, TELEPHONE, URL, "Delete"
-            };
 
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, types);
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -107,7 +110,6 @@ public class Main2Activity extends AppCompatActivity {
         mPreviewIv.setImageURI(myUri);
 
     }
-
 
     public void save_ac_btn() {
 
@@ -215,7 +217,6 @@ public class Main2Activity extends AppCompatActivity {
         finish();
     }
 
-
     protected String cleaning_string(String inputtext){
         String clean_string = "";
         String[] c_s = inputtext.split("\n");
@@ -242,6 +243,35 @@ public class Main2Activity extends AppCompatActivity {
             save_ac_btn();
         }
 
+        if (id == R.id.add_btn) {
+            add_text();
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void add_text() {
+        LinearLayout horisont_liner = new LinearLayout(this);
+        horisont_liner.setOrientation(LinearLayout.HORIZONTAL);
+        horisont_liner.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        Spinner spin = new Spinner(this);
+        spin.setId(idspinn++);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, types);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spin.setAdapter(adapter);
+
+        horisont_liner.addView(spin);
+
+        EditText et = new EditText(this);
+        LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        et.setLayoutParams(p);
+        et.setId(idtext++);
+
+        horisont_liner.addView(et);
+        ll.addView(horisont_liner);
+
     }
 }

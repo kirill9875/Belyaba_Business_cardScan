d3bc69f11162e0b4b76c084695148ad01a5506dd
Kirill <kirill1098@gmail.com>
2019-02-17 04:04:06 +0300

    реализоавана кнопка в 1активити + удалить + скролл

--- app/src/main/java/com/blogspot/atifsoftwares/imagetotextapp/MainActivity.java
+++ app/src/main/java/com/blogspot/atifsoftwares/imagetotextapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
@@ -81,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
 
         LinearLayout main = (LinearLayout)findViewById(R.id.main_layout);
 
+
         DB = dbHelper.getWritableDatabase();
 
 
@@ -131,6 +133,20 @@ public class MainActivity extends AppCompatActivity {
         } else {
             cursor.close();
         }
+
+
+
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImageImportDialog();
+            }
+        });
+
+
 
     }
 
